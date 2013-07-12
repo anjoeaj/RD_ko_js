37f13fec3b58fa19ec2ee5695fe70797bed8ecf9
125venkat <125venkat@gmail.com>
2013-07-12 19:10:00 +0530

    dynamic header

--- scripts.js
+++ scripts.js
@@ -4,7 +4,7 @@ createScreen = function(screenDetails)
 {
 	$.mobile.changePage(screenDetails.screenId+".html", { transition: "slideup", changeHash: true });
 	
-	$(document).on('pageinit', '#page1', function()
+	$(document).on('pageinit', '#'+screenDetails.screenId, function()
 	{       
   		 createHeader(screenDetails.screenId,screenDetails.header);
 		 createContent(screenDetails.content);
@@ -15,12 +15,20 @@ createScreen = function(screenDetails)
 
 createHeader = function(screenId,header)
 {
-	var headerText= header.title;
-  	$("#page1").append("<h1>header.title</h1>");
-  	$("#page1").append("<a  data-role='button' onClick='goToSecondPage()'  data-transition='slide'>Go back to second page</a>").trigger( "create" );
-  		 
+	//var mainDiv;
+	//var leftDiv;
+	//var middleDiv;
+	//var rightDiv;
+  	
+  	$.get("settingsHeader.html", function (data) 
+  	{
+  		$("#"+screenId).append(data).trigger("pagecreate");
+  		$("#"+screenId +" #headerTitle").append(document.createTextNode(header.title)).trigger("create");
+    });
+  	
 };
 
+   
 createContent = function(content)
 {
 	
